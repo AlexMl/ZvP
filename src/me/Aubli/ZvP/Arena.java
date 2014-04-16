30ad1937808af762534cdd772f426b010649ce66
AlexMl <alexandermlinzk@gmail.com>
2014-04-16 20:06:28 +0200

    Created Gamemanager, Lobby and Arena instances

--- src/me/Aubli/ZvP/Arena.java
+++ src/me/Aubli/ZvP/Arena.java
@@ -1,5 +1,227 @@
 package me.Aubli.ZvP;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+
 public class Arena {
 
+	private File arenaFile;
+	private FileConfiguration arenaConfig;
+	
+	private int arenaID;
+	
+	private World arenaWorld;
+	private Location minLoc;
+	private Location maxLoc;
+	
+	private boolean isOnline;
+	private boolean running;
+	
+	private ArrayList<ZvPPlayer> players;
+	
+	
+	public Arena(int ID, String arenaPath, Location min, Location max){
+		
+		this.arenaID = ID;
+		
+		this.arenaWorld = min.getWorld();
+		this.minLoc = min.clone();
+		this.maxLoc = max.clone();
+		
+		isOnline = true;
+		running = false;
+		
+		arenaFile = new File(arenaPath + "/" + ID + ".yml");
+		arenaConfig = YamlConfiguration.loadConfiguration(arenaFile);
+		
+		players = new ArrayList<ZvPPlayer>();
+		
+		try {
+			arenaFile.createNewFile();
+			save();
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+	}
+	
+	public Arena(File arenaFile){
+		this.arenaFile = arenaFile;
+		this.arenaConfig = YamlConfiguration.loadConfiguration(arenaFile);
+		
+		this.arenaID = arenaConfig.getInt("arena.ID");
+		this.arenaWorld = Bukkit.getWorld(arenaConfig.getString("arena.Location.world"));
+		
+		this.isOnline = arenaConfig.getBoolean("arena.Online");
+		this.running = false;
+		
+		this.minLoc = new Location(arenaWorld, 
+				arenaConfig.getInt("arena.Location.min.X"), 
+				arenaConfig.getInt("arena.Location.min.Y"),
+				arenaConfig.getInt("arena.Location.min.Z"));
+		this.maxLoc = new Location(arenaWorld, 
+				arenaConfig.getInt("arena.Location.max.X"), 
+				arenaConfig.getInt("arena.Location.max.Y"),
+				arenaConfig.getInt("arena.Location.max.Z"));
+		
+		this.players = new ArrayList<ZvPPlayer>();
+	}
+	
+	
+	void save() throws IOException{	
+		arenaConfig.set("arena.ID", arenaID);
+		arenaConfig.set("arena.Online", isOnline);
+		
+		arenaConfig.set("arena.Location.world", arenaWorld.getName());
+		arenaConfig.set("arena.Location.min.X", minLoc.getBlockX());
+		arenaConfig.set("arena.Location.min.Y", minLoc.getBlockY());
+		arenaConfig.set("arena.Location.min.Z", minLoc.getBlockZ());
+		
+		arenaConfig.set("arena.Location.max.X", maxLoc.getBlockX());
+		arenaConfig.set("arena.Location.max.Y", maxLoc.getBlockY());
+		arenaConfig.set("arena.Location.max.Z", maxLoc.getBlockZ());
+			
+		arenaConfig.save(arenaFile);			
+	}
+	
+	void delete(){
+		this.arenaFile.delete();
+	}
+	
+	
+	public int getID(){
+		return arenaID;
+	}
+	
+	public World getWorld(){
+		return arenaWorld;
+	}
+	
+	public Location getMin(){
+		return minLoc;
+	}
+	
+	public Location getMax(){
+		return maxLoc;
+	}	
+	
+	public ZvPPlayer[] getPlayers(){
+		
+		ZvPPlayer[] parray = new ZvPPlayer[players.size()];
+		
+		for(int i=0;i<players.size();i++){
+			parray[i] = players.get(i);
+		}
+		return parray;
+	}
+	
+	private Entity[] getEntities(){
+		List<Entity> eList = new ArrayList<Entity>();
+		Entity[] entities;
+		
+		Chunk minC = minLoc.getChunk();
+		Chunk maxC = maxLoc.getChunk();
+		
+		int minX = minC.getX();
+		int minZ = minC.getZ();
+		int maxX = maxC.getX();
+		int maxZ = maxC.getZ();		
+	
+		for(int x = minX;x<=maxX;x++){
+			for(int z = minZ;z<=+maxZ;z++){
+				Chunk entityChunk = getWorld().getChunkAt(x, z);				
+				for(Entity e : entityChunk.getEntities()){
+					eList.add(e);
+				}
+			}
+		}
+		
+		entities = new Entity[eList.size()];
+		for(int i=0;i<eList.size();i++){
+			entities[i] = eList.get(i);
+		}
+		return entities;
+	}
+	
+	public int getZombies(){
+		int zombies = 0;
+		for(Entity e : getEntities()){
+			if(e instanceof Zombie){
+				zombies++;
+			}
+		}
+		return zombies;
+	}
+	
+	
+	public boolean isOnline(){
+		return isOnline;
+	}
+	
+	public boolean isRunning(){
+		return running;
+	}
+	
+	
+	public boolean containsPlayer(Player player){		
+		for(ZvPPlayer zp : getPlayers()){
+			if(zp.getUuid() == player.getUniqueId()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public boolean addPlayer(ZvPPlayer player){
+		if(!players.contains(player)){
+			players.add(player);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean removePlayer(ZvPPlayer player){
+		if(players.contains(player)){
+			players.remove(player);
+			return true;
+		}
+		return false;
+	}
+	
+	public void start(){
+		this.running = true;
+		
+	}
+	
+	public void stop(){		
+		for(ZvPPlayer zp : getPlayers()){
+			zp.reset();
+			removePlayer(zp);
+		}
+		clearArena();
+		this.running = false;
+	}
+	
+	public void clearArena(){		
+		for(Entity e : getEntities()){
+			if(e instanceof Zombie || e instanceof Item || e instanceof ExperienceOrb){
+				e.remove();
+			}			
+		}
+	}
 }
--- src/me/Aubli/ZvP/GameManager.java
+++ src/me/Aubli/ZvP/GameManager.java
@@ -1,5 +1,263 @@
 package me.Aubli.ZvP;
 
-public class GameManager {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class GameManager {
+	
+	private static GameManager manager;
+	private ZvP plugin;
+	
+	private String arenaPath;
+	private String lobbyPath;
+	
+	private ArrayList<Lobby> lobbys;
+	private ArrayList<Arena> arenas;
+	
+	
+	public GameManager(){
+		manager = this;
+		plugin = ZvP.getInstance();
+		
+		arenaPath = plugin.getDataFolder().getPath() + "/Arenas";
+		lobbyPath = plugin.getDataFolder().getPath() + "/Lobbys";
+		
+		if(!new File(arenaPath).exists() || !new File(lobbyPath).exists()){
+			new File(arenaPath).mkdirs();
+			new File(lobbyPath).mkdirs();
+		}		
+		
+		loadConfig();
+	}
+	
+	public static GameManager getManager(){
+		return manager;
+	}
+	
+	public void loadConfig(){
+		
+		lobbys = new ArrayList<Lobby>();
+		arenas = new ArrayList<Arena>();
+		
+		loadArenas();
+		loadLobbys();
+	}
+	
+	public void saveConfig(){
+		try{
+			saveArenas();
+			saveLobbys();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private void loadArenas(){		
+		for(int i=0;i<new File(arenaPath).listFiles().length;i++){			
+			Arena arena = new Arena(new File(arenaPath).listFiles()[i]);
+			
+			if(arena.getWorld()!=null){
+				arenas.add(arena);
+			}			
+		}
+	}
+	
+	private void loadLobbys(){
+		for(int i=0;i<new File(lobbyPath).listFiles().length;i++){
+			Lobby lobby = new Lobby(new File(lobbyPath).listFiles()[i]);
+			
+			if(lobby.getWorld()!=null){
+				lobbys.add(lobby);
+			}
+		}
+	}
+	
+	private void saveArenas() throws IOException{
+		for(int i=0;i<getArenas().length;i++){
+			getArenas()[i].save();
+		}
+	}
+	
+	private void saveLobbys() throws IOException{
+		for(int i=0;i<getLobbys().length;i++){
+			getLobbys()[i].save();
+		}
+	}
+	
+	
+	public int getNewID(String path){
+		
+		File folder = new File(path);
+		if(folder.listFiles().length==0){
+			return 1;
+		}else{
+			
+			int[] fileIds = new int[folder.listFiles().length];
+			
+			for(int i=0;i<fileIds.length;i++){
+				fileIds[i] = Integer.parseInt(folder.listFiles()[i].getName().split(".ym")[0]);
+			}
+			
+			Arrays.sort(fileIds);
+			
+			for(int k=0;k<fileIds.length;k++){
+				if(fileIds[k]!=(k+1)){
+					return (k+1);
+				}
+			}
+			return fileIds.length+1;
+		}
+		
+	}
+	
+	
+	public Arena[] getArenas(){
+		Arena[] array = new Arena[arenas.size()];
+		
+		for(int i=0;i<arenas.size();i++){
+			array[i] = arenas.get(i);
+		}
+		return array;
+	}
+	
+	public Lobby[] getLobbys(){
+		Lobby[] array = new Lobby[lobbys.size()];
+		
+		for(int i=0;i<lobbys.size();i++){
+			array[i] = lobbys.get(i);
+		}
+		return array;
+	}
+
+	public Arena getArena(int ID){
+		for(Arena a : getArenas()){
+			if(a.getID()==ID){
+				return a;
+			}
+		}
+		return null;
+	}
+	
+	public Lobby getLobby(int ID){
+		for(Lobby l : getLobbys()){
+			if(l.getID()==ID){
+				return l;
+			}
+		}
+		return null;
+	}
+	
+	
+	public boolean addArena(Location min, Location max){		
+		
+		if(min.getWorld().equals(max.getWorld())){
+			
+			double tempX;
+			double tempY;		
+			double tempZ;
+			
+			if(min.getX()>max.getX()){
+				tempX = min.getX();
+				min.setX(max.getX());
+				max.setX(tempX);
+			}
+			
+			if(min.getY()>max.getY()){
+				tempY = min.getY();
+				min.setY(max.getY());
+				max.setY(tempY);
+			}
+			
+			if(min.getZ()>max.getZ()){
+				tempZ = min.getZ();
+				min.setZ(max.getZ());
+				max.setZ(tempZ);
+			}			
+			
+			Arena a = new Arena(getNewID(arenaPath), arenaPath, min.clone(), max.clone());
+			arenas.add(a);
+			return true;
+		}
+		return false;
+	}
+	
+	public void addLobby(Location loc){
+		Lobby l = new Lobby(getNewID(lobbyPath), lobbyPath, loc.clone());
+		lobbys.add(l);
+	}
+	
+	
+	public void removeArena(Arena arena){
+		arenas.remove(arena);
+		arena.delete();
+	}
+	
+	public void removeLobby(Lobby lobby){
+		lobbys.remove(lobby);
+		lobby.delete();
+	}
+
+	
+	public void startGame(Arena a){
+		
+	}
+	
+	public void stopGame(Arena a){
+		
+	}
+	
+	public void stopGames(){
+		for(Arena a : getArenas()){
+			
+		}
+	}
+	
+	public boolean isInGame(Player player){		
+		for(Arena a : getArenas()){
+			return a.containsPlayer(player);
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
