b232eb25552c9bdfaf65f4f7b4f996b818aad6cf
AlexMl <alexandermlinzk@gmail.com>
2014-05-24 00:28:09 +0200

    Added test content

--- src/me/Aubli/ZvP/GameRunnable.java
+++ src/me/Aubli/ZvP/GameRunnable.java
@@ -1,13 +1,16 @@
 package me.Aubli.ZvP;
 
+import me.Aubli.ZvP.GameManager.ArenaStatus;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class GameRunnable extends BukkitRunnable{
 	
 	public GameRunnable(Arena arena, int startDelay){
 		this.arena = arena;
-		this.startDelay = startDelay;
+		this.startDelay = 5;//startDelay;
 	}
 	
 	private Arena arena;	
@@ -15,12 +18,38 @@ public class GameRunnable extends BukkitRunnable{
 	
 	private int seconds = 0;
 	
+	private int currentRound = 0;
+	private int currentWave = 0;
+	
+	private boolean firstSpawn;
+	
 	@Override
 	public void run() {
-		Bukkit.broadcastMessage("Arena: " + arena.getID() + "\nPlayers: " + arena.getPlayers().toString() + "\nSeconds: " + seconds);
-		if(seconds>=startDelay){
+		//Bukkit.broadcastMessage("\nTaskID: " + this.getTaskId() + "\nArena: " + arena.getID() + "\nPlayers: " + arena.getPlayers().toString() + "\nSeconds: " + seconds);
+		if(seconds<=startDelay){
+			arena.setStatus(ArenaStatus.WAITING);
+			arena.setPlayerLevel(startDelay-seconds);
 			
+			firstSpawn = true;
+		}else if(seconds > startDelay) {
+			currentRound++;
+			currentWave++;
+			
+			arena.setStatus(ArenaStatus.RUNNING);			
+			arena.setPlayerLevel(0);
+		
+			if(firstSpawn) {
+				arena.spawnZombies(currentRound*currentWave*30);// - (int)(currentRound*currentWave*30*0.2));
+				firstSpawn = false;
+			}else {
+				arena.spawnZombies(1);
+				firstSpawn = true;
+			}
+		
 		}
+
+		arena.sendMessage("Arena: " + arena.getID() + " : " + ChatColor.RED + arena.getStatus().toString() + " ; " + startDelay + ":" + seconds);
+		
 		seconds++;
 	}
 
