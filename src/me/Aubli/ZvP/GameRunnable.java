2acc578bca90dfb41203d97673bb5eda7f025b70
AlexMl <alexandermlinzk@gmail.com>
2014-05-24 21:37:35 +0200

    Worked on Game itself

--- src/me/Aubli/ZvP/GameRunnable.java
+++ src/me/Aubli/ZvP/GameRunnable.java
@@ -2,26 +2,30 @@ package me.Aubli.ZvP;
 
 import me.Aubli.ZvP.GameManager.ArenaStatus;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class GameRunnable extends BukkitRunnable{
 	
-	public GameRunnable(Arena arena, int startDelay){
+	public GameRunnable(Arena arena, int startDelay, int saveTime, int magicSpawnRate){
 		this.arena = arena;
 		this.startDelay = 5;//startDelay;
+		this.spawnRate = magicSpawnRate;
+		this.saveTime = saveTime;
 	}
 	
 	private Arena arena;	
-	private int startDelay;
+	
+	private final int startDelay;
+	private final int saveTime;
 	
 	private int seconds = 0;
+	private int seconds2 = 0;
 	
-	private int currentRound = 0;
-	private int currentWave = 0;
+	private int spawnRate;
 	
 	private boolean firstSpawn;
+	private boolean spawnZombies;
 	
 	@Override
 	public void run() {
@@ -32,24 +36,62 @@ public class GameRunnable extends BukkitRunnable{
 			
 			firstSpawn = true;
 		}else if(seconds > startDelay) {
-			currentRound++;
-			currentWave++;
-			
-			arena.setStatus(ArenaStatus.RUNNING);			
-			arena.setPlayerLevel(0);
-		
-			if(firstSpawn) {
-				arena.spawnZombies(currentRound*currentWave*30);// - (int)(currentRound*currentWave*30*0.2));
-				firstSpawn = false;
-			}else {
-				arena.spawnZombies(1);
+			if(arena.getRound()==0 && arena.getWave()==0) { //start
+				arena.setStatus(ArenaStatus.RUNNING);			
+				arena.setPlayerLevel(0);
+				arena.setRound(1);
+				arena.setWave(1);
+				
 				firstSpawn = true;
+				spawnZombies = false;
 			}
-		
+			
+			if(firstSpawn) {
+				arena.spawnZombies(arena.getRound()*arena.getWave()*spawnRate - (int)(arena.getRound()*arena.getWave()*spawnRate*0.3));
+				firstSpawn = false;
+				spawnZombies = true;
+			}else {
+				if(arena.getLivingZombies()<arena.getRound()*arena.getWave()*spawnRate && spawnZombies) {
+					double spawn = arena.getRound()*arena.getWave()*spawnRate;
+					System.out.println("Missing: " + (spawn-arena.getLivingZombies()));
+						
+					if(spawn-arena.getLivingZombies() >= ((int)(spawn*0.17)) && (int)(spawn*0.10)>0) {
+						arena.spawnZombies((int)(spawn*0.10));
+						System.out.println("10% " + (int)(spawn*0.10));
+					}else if(spawn-arena.getLivingZombies() >= ((int)(spawn*0.12)) && (int)(spawn*0.06)>0) {
+						arena.spawnZombies((int)(spawn*0.06));
+						System.out.println("6% " + (int)(spawn*0.06));						
+					}else  if(spawn-arena.getLivingZombies() >= ((int)(spawn*0.08)) && (int)(spawn*0.02)>0) {
+						arena.spawnZombies((int)(spawn*0.02));
+						System.out.println("2% " + (int)(spawn*0.02));
+					}else {
+						arena.spawnZombies(1);	
+						System.out.println(1);
+					}
+				}else {
+					spawnZombies = false;
+					firstSpawn = false;
+				}
+			}
+			
+			if(firstSpawn==false && spawnZombies==false) {
+				if(arena.getLivingZombies()==0) {
+					if(seconds2<=saveTime) {					
+						arena.sendMessage(saveTime-seconds2 + " Seconds left!");
+						seconds2++;
+					}else {
+						arena.sendMessage("next wave!");
+						arena.next();
+						firstSpawn = true;
+						seconds2 = 0;
+					}
+				}
+			}
+			
 		}
 
-		arena.sendMessage("Arena: " + arena.getID() + " : " + ChatColor.RED + arena.getStatus().toString() + " ; " + startDelay + ":" + seconds);
-		
+		if(arena.getLivingZombies()>0)arena.sendMessage("Arena: " + arena.getID() + " : " + ChatColor.RED + arena.getStatus().toString() + "; " + ChatColor.RESET + arena.getRound() + ":" + arena.getWave() + " Z:" + arena.getLivingZombies());
+		arena.getWorld().setTime(15000L);
 		seconds++;
 	}
 
