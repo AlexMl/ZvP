70d1fe3aac7a0f1a86891c1b64e2caca3705f18d
AlexMl <alexandermlinzk@gmail.com>
2014-06-17 20:28:18 +0200

    Added some messages

--- src/me/Aubli/ZvP/Translation/DefaultTranslation.java
+++ src/me/Aubli/ZvP/Translation/DefaultTranslation.java
@@ -10,8 +10,22 @@ public class DefaultTranslation extends ListResourceBundle{
 	}
 
 	private Object[][] contents = {
+		{"missing_Permission", "You do not have enough permission for that!"},
+		{"config_saved", "Config successfully saved!"},
+		{"config_reloaded", "Config successfully reloaded!"},
+		{"leave_game", "You have left arena %arena%!"},
+		{"player_left", "Player %player% has left the game!"},
+		{"not_in_game", "You are not in a game!"},
+		{"arena_stop_all", "All arenas halted!"},
+		{"arena_stop", "Arena %arena% stoped!"},
+		{"lobby_created", "Lobby saved!"},
+		{"arena_created", "Arena saved!"},
+		{"lobby_removed", "Lobby removed from Config!"},
+		{"arena_removed", "Arena removed from Config!"},
+		{"arena_not_available", "This arena is not available!"},
 		{"", ""},
-		{"", ""}
+		{"", ""},
+		
 	};
 	
 }
