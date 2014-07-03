148f712212f2f7306ce8d2d7cfdbeb24875873b6
AlexMl <alexandermlinzk@gmail.com>
2014-07-03 16:45:03 +0200

    Udated ItemStorage to support ShopItem

--- src/org/util/ItemStorageUtil/ItemStorage.java
+++ src/org/util/ItemStorageUtil/ItemStorage.java
@@ -3,7 +3,11 @@ package org.util.ItemStorageUtil;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import me.Aubli.ZvP.Shop.ShopItem;
 
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -21,35 +25,50 @@ public class ItemStorage {
 	 * @param content			The Array of ItemStacks
 	 */
 	public static void saveItemsToFile(File saveFile, String configSection, ItemStack[] content) {
+		// - { id: IRON_SWORD, amount: 1, data: 0, ench: {DAMAGE_ALL:1} }
 		
-		FileConfiguration fileConfig = YamlConfiguration.loadConfiguration(saveFile);
-		
+		FileConfiguration fileConfig = YamlConfiguration.loadConfiguration(saveFile);		
 		List<String> itemList = new ArrayList<String>();
 		
 		for(ItemStack item : content) {
 			if(item!=null && item.getType()!=Material.AIR) {
-				String enchString = "ench: {";
-				if(item.getEnchantments().size()>1) {
-					for(int i=0;i<item.getEnchantments().size()-1;i++) {
-						Enchantment ench = (Enchantment) item.getEnchantments().keySet().toArray()[i];
-						enchString += ench.getName() + ":" + item.getEnchantments().get(ench);
-						enchString += ", ";
-					}				
-					enchString += ((Enchantment)item.getEnchantments().keySet().toArray()[item.getEnchantments().size()-1]).getName() + ":" + item.getEnchantments().get(item.getEnchantments().keySet().toArray()[item.getEnchantments().size()-1]);
-
-				}else if(item.getEnchantments().size()==1) {
-					Enchantment ench = (Enchantment) item.getEnchantments().keySet().toArray()[0];
-					enchString += ench.getName() + ":" + item.getEnchantments().get(ench);
-				}
-				enchString += "}";
-				
-				String itemString = "id: " + item.getType().toString() + ", amount: " + item.getAmount() + ", data: " + item.getDurability() + ", " + enchString;
+				String enchString = translateEnchantment(item);				
+				String itemString = generateItemString(enchString, item);
 				System.out.println(itemString);
 				itemList.add(itemString);		
 			}
 		}
 		
-		// - { id: IRON_SWORD, amount: 1, data: 0, ench: {DAMAGE_ALL:1} }
+		fileConfig.set(configSection, itemList);
+
+		try {
+			fileConfig.save(saveFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Save an Array of ShopItems to an specific file in a specific config section.
+	 * 
+	 * @param saveFile			The file to save
+	 * @param configSection		The config section
+	 * @param content			The Array of Shopitems
+	 */
+	public static void saveItemsToFile(File saveFile, String configSection, ShopItem[] content) {
+		// - { id: IRON_SWORD, amount: 1, data: 0, ench: {DAMAGE_ALL:1}, Price: 5.0}
+		
+		FileConfiguration fileConfig = YamlConfiguration.loadConfiguration(saveFile);		
+		List<String> itemList = new ArrayList<String>();
+		
+		for(ShopItem item : content) {
+			if(item.getItem()!=null && item.getType()!=Material.AIR) {
+				String enchString = translateEnchantment(item.getItem());				
+				String itemString = generateShopItemString(enchString, item);
+				System.out.println(itemString);
+				itemList.add(itemString);		
+			}
+		}
 		
 		fileConfig.set(configSection, itemList);
 
@@ -73,52 +92,138 @@ public class ItemStorage {
 		
 		for(Object listObject : itemList) {
 
-			try {				
-					
+			try {					
 				String itemString = (String) listObject;
 				
-				Material m = Material.getMaterial(itemString.split(", ")[0].split("id: ")[1]);
-				
-				int amount = Integer.parseInt(itemString.split(", ")[1].split("amount: ")[1]);
-				
-				short data = Short.parseShort(itemString.split(", ")[2].split("data: ")[1]);
-				
-				String enchString = itemString.split("ench: ")[1].replace("{", "").replace("}", "");
-				
-				String[] enchantments = enchString.split(", ");
+				Material m = parseMaterial(itemString);
+				int amount = parseAmount(itemString);
+				short data = parseData(itemString);				
 				
 				ItemStack item = new ItemStack(m, amount, data);
-				
-				if(enchantments.length>0) {
-					for(String ench : enchantments) {
-						if(!ench.isEmpty()) {
-							System.out.println("E: " + ench);
-							Enchantment itemEnchantment = Enchantment.getByName(ench.split(":")[0]);
-							int level = Integer.parseInt(ench.split(":")[1]);
-							item.addUnsafeEnchantment(itemEnchantment, level);
-						}
-					}
-				}
-				
-				System.out.println(item);
-				
-				itemArray.add(item);
-				
+				applyEnchantment(item, itemString);
+				itemArray.add(item);				
 			}catch(Exception e) {
 				throw new Exception(e.getMessage() + " in Object: " + (String)listObject);
 			}
+		}		
+		return toArray(itemArray);
+	}
+	
+	/**
+	 * Get an Array of ShopItems from an List of items. Used in cooperation with the saveItemsToFile method.
+	 *  
+	 * @param itemList				The list of items
+	 * @return {@link ItemStack[]} 	The array of {@link ItemStack}s with data, {@link Enchantment}, amount
+	 * @throws Exception			Throws {@link Exception} if a listentry is not correct.
+	 */
+	public static ShopItem[] getShopItemsFromFile(List<?> itemList) throws Exception {
+		
+		Map<ItemStack, Double> itemMap = new HashMap<ItemStack, Double>();
+		
+		for(Object listObject : itemList) {
+
+			try {					
+				String itemString = (String) listObject;
+				
+				Material m = parseMaterial(itemString);
+				int amount = parseAmount(itemString);
+				short data = parseData(itemString);				
+				double price = parsePrice(itemString);
+				
+				ItemStack item = new ItemStack(m, amount, data);
+				applyEnchantment(item, itemString);
+				itemMap.put(item, price);				
+			}catch(Exception e) {
+				throw new Exception(e.getMessage() + " in Object: " + (String)listObject);
+			}
+		}		
+		return toShopItems(itemMap);
+	}
+	
+	private static String generateItemString(String enchString, ItemStack item) {
+		return "id: " + item.getType().toString() + ", amount: " + item.getAmount() + ", data: " + item.getDurability() + ", " + enchString;
+	}
+	
+	private static String generateShopItemString(String enchString, ShopItem item) {
+		return "id: " + item.getType().toString() + ", amount: " + item.getItem().getAmount() + ", data: " + item.getItem().getDurability() + ", Price: " + item.getPrice() + ", " + enchString;
+	}
+	
+	private static String translateEnchantment(ItemStack item) {
+		
+		String enchString = "ench: {";
+		if(item.getEnchantments().size()>1) {
+			for(int i=0;i<item.getEnchantments().size()-1;i++) {
+				Enchantment ench = (Enchantment) item.getEnchantments().keySet().toArray()[i];
+				enchString += ench.getName() + ":" + item.getEnchantments().get(ench);
+				enchString += ", ";
+			}				
+			enchString += ((Enchantment)item.getEnchantments().keySet().toArray()[item.getEnchantments().size()-1]).getName() + ":" + item.getEnchantments().get(item.getEnchantments().keySet().toArray()[item.getEnchantments().size()-1]);
+
+		}else if(item.getEnchantments().size()==1) {
+			Enchantment ench = (Enchantment) item.getEnchantments().keySet().toArray()[0];
+			enchString += ench.getName() + ":" + item.getEnchantments().get(ench);
 		}
-			
-		// - { id: IRON_SWORD, amount: 1, data: 0, ench: {DAMAGE_ALL:1} }
-			
-		ItemStack[] returnStack = new ItemStack[itemArray.size()];
-			
+		enchString += "}";
+		
+		return enchString;
+	}
+	
+	
+	private static Material parseMaterial(String itemString) {
+		return Material.getMaterial(itemString.split(", ")[0].split("id: ")[1]);
+	}
+	
+	private static int parseAmount(String itemString) throws NumberFormatException {
+		return Integer.parseInt(itemString.split(", ")[1].split("amount: ")[1]);
+	}
+	
+	private static short parseData(String itemString) throws NumberFormatException {
+		return Short.parseShort(itemString.split(", ")[2].split("data: ")[1]);
+	}
+	
+	private static double parsePrice(String itemString) throws NumberFormatException {
+		return Double.parseDouble(itemString.split(", ")[3].split("Price: ")[1]);
+	}
+	
+	private static ItemStack applyEnchantment(ItemStack item, String itemString) throws Exception {
+		String enchString = itemString.split("ench: ")[1].replace("{", "").replace("}", "");
+		
+		String[] enchantments = enchString.split(", ");
+		
+		if(enchantments.length>0) {
+			for(String ench : enchantments) {
+				if(!ench.isEmpty()) {
+					System.out.println("E: " + ench);
+					Enchantment itemEnchantment = Enchantment.getByName(ench.split(":")[0]);
+					int level = Integer.parseInt(ench.split(":")[1]);
+					item.addUnsafeEnchantment(itemEnchantment, level);
+				}
+			}
+		}
+		return item;
+	}
+	
+	private static ItemStack[] toArray(ArrayList<ItemStack> items) {
+		ItemStack[] returnStack = new ItemStack[items.size()];
+		
 		for (int i = 0; i < returnStack.length; i++) {
-			returnStack[i] = itemArray.get(i);
+			returnStack[i] = items.get(i);
 		}	
 			
 		return returnStack;
 	}
-	
-	
+
+	private static ShopItem[] toShopItems(Map<ItemStack, Double> items) {
+		
+		ShopItem[] shopItems = new ShopItem[items.size()];		
+		
+		int i = 0;
+		
+		for(ItemStack item : items.keySet()) {
+			shopItems[i] = new ShopItem(item, items.get(item));
+			i++;
+		}
+		
+		return shopItems;
+	}
 }
