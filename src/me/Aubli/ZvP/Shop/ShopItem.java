f7c0ee1ce3f1fbf1546d8fafc365d1c060d55c83
AlexMl <alexandermlinzk@gmail.com>
2014-07-03 16:45:14 +0200

    Updated Shop system

--- src/me/Aubli/ZvP/Shop/ShopItem.java
+++ src/me/Aubli/ZvP/Shop/ShopItem.java
@@ -1,5 +1,37 @@
 package me.Aubli.ZvP.Shop;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 public class ShopItem {
 
+	private final ItemStack item;
+
+	private double price;
+	
+	
+	public ShopItem(ItemStack item, double price) {
+		this.item = item.clone();
+		this.price = price;
+	}
+	
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public ItemStack getItem() {
+		return this.item;
+	}
+
+
+	public Material getType() {
+		return getItem().getType();
+	}
+	
 }
--- src/me/Aubli/ZvP/Shop/ShopManager.java
+++ src/me/Aubli/ZvP/Shop/ShopManager.java
@@ -1,30 +1,63 @@
-package me.Aubli.ZvP.Game;
+package me.Aubli.ZvP.Shop;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
 
+import me.Aubli.ZvP.ZvP;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+import org.util.ItemStorageUtil.ItemStorage;
 
 public class ShopManager {
 
-	private HashMap<ItemStack, Double> items;
+	private File itemFile;
+	private FileConfiguration itemConfig;
+	
+	private ShopItem[] items;
 	
 	
 	public ShopManager() {
-		items = new HashMap<ItemStack, Double>();
+		
+		itemFile = new File(ZvP.getInstance().getDataFolder().getPath() + "/Shop/items.yml");
+		itemConfig = YamlConfiguration.loadConfiguration(itemFile);
+		
+		if(!itemFile.exists()) {
+			try {
+				itemFile.getParentFile().mkdirs();			
+				itemFile.createNewFile();
+				writeDefaults();
+			} catch (IOException e) {				
+				e.printStackTrace();
+			}
+		}
+		
+		items = loadItems();
 	}
 	
 	
-	private Map<ItemStack, Double> loadItems(){
+	private void writeDefaults() {
+		
+	}
+	
+	
+	private ShopItem[] loadItems(){
+		try {
+			return ItemStorage.getShopItemsFromFile(itemConfig.getList("items"));
+		} catch (Exception e) {
+			ZvP.log.log(Level.WARNING, "Error while loading Item from shop configuration!\nError: " + e.getMessage() + " in File " + itemFile.getPath(), e);			
+		}
 		return null;
 	}
 	
 	
 	//Shop control
 	public ItemStack[] getItems() {
+		
 		return null;
 	}
-	
 	
 	
 	
