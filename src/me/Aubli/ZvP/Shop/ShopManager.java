20de88caa2b684b665c51ae3cc7c8982bd82ccd9
AlexMl <alexandermlinzk@gmail.com>
2014-07-04 16:56:16 +0200

    Added default items

--- src/me/Aubli/ZvP/Shop/ShopManager.java
+++ src/me/Aubli/ZvP/Shop/ShopManager.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 
 import me.Aubli.ZvP.ZvP;
 
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
@@ -48,6 +49,95 @@ public class ShopManager {
 	
 	private void writeDefaults() {
 		
+		ShopItem[] defaultItems = new ShopItem[33];
+		
+		ItemStack item;
+		ItemCategory cat;
+		
+		//Food
+		cat = ItemCategory.FOOD;		
+		item = new ItemStack(Material.APPLE);
+		defaultItems[0] = new ShopItem(item, cat, 2.0);
+		item = new ItemStack(Material.BAKED_POTATO);
+		defaultItems[1] = new ShopItem(item, cat, 4.0);
+		item = new ItemStack(Material.BREAD);
+		defaultItems[2] = new ShopItem(item, cat, 3.5);
+		item = new ItemStack(Material.CAKE);
+		defaultItems[3] = new ShopItem(item, cat, 9.0);
+		item = new ItemStack(Material.COOKED_BEEF);
+		defaultItems[4] = new ShopItem(item, cat, 5.0);
+		item = new ItemStack(Material.COOKED_CHICKEN);
+		defaultItems[5] = new ShopItem(item, cat, 5.0);
+		item = new ItemStack(Material.COOKED_FISH);
+		defaultItems[6] = new ShopItem(item, cat, 6.0);
+		item = new ItemStack(Material.COOKIE);
+		defaultItems[7] = new ShopItem(item, cat, 1.5);
+		item = new ItemStack(Material.GRILLED_PORK);
+		defaultItems[8] = new ShopItem(item, cat, 5.0);
+		item = new ItemStack(Material.PORK);
+		defaultItems[9] = new ShopItem(item, cat, 4.5);
+		
+		
+		//Misc
+		cat = ItemCategory.MISC;
+		item = new ItemStack(Material.STICK);
+		defaultItems[10] = new ShopItem(item, cat, 1.0);
+		item = new ItemStack(Material.EXP_BOTTLE);
+		defaultItems[11] = new ShopItem(item, cat, 7.5);
+		item = new ItemStack(Material.LEATHER);
+		defaultItems[12] = new ShopItem(item, cat, 2.0);
+		item = new ItemStack(Material.GOLD_INGOT);
+		defaultItems[13] = new ShopItem(item, cat, 3.0);
+		item = new ItemStack(Material.IRON_INGOT);
+		defaultItems[14] = new ShopItem(item, cat, 3.5);
+		
+		
+		//Armor
+		cat = ItemCategory.ARMOR;
+		item = new ItemStack(Material.LEATHER_HELMET);
+		defaultItems[15] = new ShopItem(item, cat, 3.0);
+		item = new ItemStack(Material.LEATHER_CHESTPLATE);
+		defaultItems[16] = new ShopItem(item, cat, 4.0);
+		item = new ItemStack(Material.LEATHER_LEGGINGS);
+		defaultItems[17] = new ShopItem(item, cat, 4.0);
+		item = new ItemStack(Material.LEATHER_BOOTS);
+		defaultItems[18] = new ShopItem(item, cat, 3.0);
+		
+		item = new ItemStack(Material.IRON_HELMET);
+		defaultItems[19] = new ShopItem(item, cat, 4.0);
+		item = new ItemStack(Material.IRON_CHESTPLATE);
+		defaultItems[20] = new ShopItem(item, cat, 5.5);
+		item = new ItemStack(Material.IRON_LEGGINGS);
+		defaultItems[21] = new ShopItem(item, cat, 5.0);
+		item = new ItemStack(Material.IRON_BOOTS);
+		defaultItems[22] = new ShopItem(item, cat, 3.0);
+		
+		item = new ItemStack(Material.GOLD_HELMET);
+		defaultItems[23] = new ShopItem(item, cat, 3.5);
+		item = new ItemStack(Material.GOLD_CHESTPLATE);
+		defaultItems[24] = new ShopItem(item, cat, 5.0);
+		item = new ItemStack(Material.GOLD_LEGGINGS);
+		defaultItems[25] = new ShopItem(item, cat, 4.5);
+		item = new ItemStack(Material.GOLD_BOOTS);
+		defaultItems[26] = new ShopItem(item, cat, 3.0);
+		
+		
+		//weapons
+		cat = ItemCategory.WEAPONS;
+		item = new ItemStack(Material.DIAMOND_AXE);
+		defaultItems[27] = new ShopItem(item, cat, 9.0);
+		item = new ItemStack(Material.DIAMOND_SWORD);
+		defaultItems[28] = new ShopItem(item, cat, 10.0);
+		item = new ItemStack(Material.IRON_AXE);
+		defaultItems[29] = new ShopItem(item, cat, 5.0);
+		item = new ItemStack(Material.IRON_SWORD);
+		defaultItems[29] = new ShopItem(item, cat, 6.0);
+		item = new ItemStack(Material.GOLD_SWORD);
+		defaultItems[30] = new ShopItem(item, cat, 4.0);
+		item = new ItemStack(Material.WOOD_SWORD);
+		defaultItems[31] = new ShopItem(item, cat, 2.0);		
+		
+		ItemStorage.saveItemsToFile(itemFile, "items", defaultItems);
 	}
 	
 	
