aa5254e09f9314efafd1ee65e48cc34424b3e66f
AlexMl <alexandermlinzk@gmail.com>
2014-07-07 14:54:42 +0200

    Added new Items and methods

--- src/me/Aubli/ZvP/Shop/ShopManager.java
+++ src/me/Aubli/ZvP/Shop/ShopManager.java
@@ -2,6 +2,8 @@ package me.Aubli.ZvP.Shop;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import me.Aubli.ZvP.ZvP;
@@ -9,6 +11,7 @@ import me.Aubli.ZvP.ZvP;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.util.ItemStorageUtil.ItemStorage;
 
@@ -53,7 +56,7 @@ public class ShopManager {
 	
 	private void writeDefaults() {
 		
-		ShopItem[] defaultItems = new ShopItem[33];
+		ShopItem[] defaultItems = new ShopItem[37];
 		
 		ItemStack item;
 		ItemCategory cat;
@@ -78,68 +81,85 @@ public class ShopManager {
 		defaultItems[7] = new ShopItem(item, cat, 1.5);
 		item = new ItemStack(Material.GRILLED_PORK);
 		defaultItems[8] = new ShopItem(item, cat, 5.0);
-		item = new ItemStack(Material.PORK);
-		defaultItems[9] = new ShopItem(item, cat, 4.5);
 		
 		
 		//Misc
 		cat = ItemCategory.MISC;
 		item = new ItemStack(Material.STICK);
-		defaultItems[10] = new ShopItem(item, cat, 1.0);
+		defaultItems[9] = new ShopItem(item, cat, 1.0);
 		item = new ItemStack(Material.EXP_BOTTLE);
-		defaultItems[11] = new ShopItem(item, cat, 7.5);
+		defaultItems[10] = new ShopItem(item, cat, 7.5);
 		item = new ItemStack(Material.LEATHER);
-		defaultItems[12] = new ShopItem(item, cat, 2.0);
+		defaultItems[11] = new ShopItem(item, cat, 2.0);
 		item = new ItemStack(Material.GOLD_INGOT);
-		defaultItems[13] = new ShopItem(item, cat, 3.0);
+		defaultItems[12] = new ShopItem(item, cat, 3.0);
 		item = new ItemStack(Material.IRON_INGOT);
-		defaultItems[14] = new ShopItem(item, cat, 3.5);
+		defaultItems[13] = new ShopItem(item, cat, 3.5);
 		
 		
 		//Armor
 		cat = ItemCategory.ARMOR;
 		item = new ItemStack(Material.LEATHER_HELMET);
-		defaultItems[15] = new ShopItem(item, cat, 3.0);
+		defaultItems[14] = new ShopItem(item, cat, 3.0);
 		item = new ItemStack(Material.LEATHER_CHESTPLATE);
-		defaultItems[16] = new ShopItem(item, cat, 4.0);
+		defaultItems[15] = new ShopItem(item, cat, 4.0);
 		item = new ItemStack(Material.LEATHER_LEGGINGS);
-		defaultItems[17] = new ShopItem(item, cat, 4.0);
+		defaultItems[16] = new ShopItem(item, cat, 4.0);
 		item = new ItemStack(Material.LEATHER_BOOTS);
-		defaultItems[18] = new ShopItem(item, cat, 3.0);
+		defaultItems[17] = new ShopItem(item, cat, 3.0);
+		
+		item = new ItemStack(Material.CHAINMAIL_HELMET);
+		defaultItems[18] = new ShopItem(item, cat, 4.0);
+		item = new ItemStack(Material.CHAINMAIL_CHESTPLATE);
+		defaultItems[19] = new ShopItem(item, cat, 4.5);
+		item = new ItemStack(Material.CHAINMAIL_LEGGINGS);
+		defaultItems[20] = new ShopItem(item, cat, 4.5);
+		item = new ItemStack(Material.CHAINMAIL_BOOTS);
+		defaultItems[21] = new ShopItem(item, cat, 3.5);
 		
 		item = new ItemStack(Material.IRON_HELMET);
-		defaultItems[19] = new ShopItem(item, cat, 4.0);
+		defaultItems[22] = new ShopItem(item, cat, 4.0);
 		item = new ItemStack(Material.IRON_CHESTPLATE);
-		defaultItems[20] = new ShopItem(item, cat, 5.5);
+		defaultItems[23] = new ShopItem(item, cat, 5.5);
 		item = new ItemStack(Material.IRON_LEGGINGS);
-		defaultItems[21] = new ShopItem(item, cat, 5.0);
+		defaultItems[24] = new ShopItem(item, cat, 5.0);
 		item = new ItemStack(Material.IRON_BOOTS);
-		defaultItems[22] = new ShopItem(item, cat, 3.0);
+		defaultItems[25] = new ShopItem(item, cat, 3.0);
 		
 		item = new ItemStack(Material.GOLD_HELMET);
-		defaultItems[23] = new ShopItem(item, cat, 3.5);
+		defaultItems[26] = new ShopItem(item, cat, 3.5);
 		item = new ItemStack(Material.GOLD_CHESTPLATE);
-		defaultItems[24] = new ShopItem(item, cat, 5.0);
+		defaultItems[27] = new ShopItem(item, cat, 5.0);
 		item = new ItemStack(Material.GOLD_LEGGINGS);
-		defaultItems[25] = new ShopItem(item, cat, 4.5);
+		defaultItems[28] = new ShopItem(item, cat, 4.5);
 		item = new ItemStack(Material.GOLD_BOOTS);
-		defaultItems[26] = new ShopItem(item, cat, 3.0);
+		defaultItems[29] = new ShopItem(item, cat, 3.0);
 		
 		
 		//weapons
 		cat = ItemCategory.WEAPON;
 		item = new ItemStack(Material.DIAMOND_AXE);
-		defaultItems[27] = new ShopItem(item, cat, 9.0);
+		defaultItems[30] = new ShopItem(item, cat, 9.0);
 		item = new ItemStack(Material.DIAMOND_SWORD);
-		defaultItems[28] = new ShopItem(item, cat, 10.0);
+		defaultItems[31] = new ShopItem(item, cat, 10.0);
 		item = new ItemStack(Material.IRON_AXE);
-		defaultItems[29] = new ShopItem(item, cat, 5.0);
+		defaultItems[32] = new ShopItem(item, cat, 5.0);
 		item = new ItemStack(Material.IRON_SWORD);
-		defaultItems[29] = new ShopItem(item, cat, 6.0);
+		defaultItems[33] = new ShopItem(item, cat, 6.0);
 		item = new ItemStack(Material.GOLD_SWORD);
-		defaultItems[30] = new ShopItem(item, cat, 4.0);
+		defaultItems[34] = new ShopItem(item, cat, 4.0);
 		item = new ItemStack(Material.WOOD_SWORD);
-		defaultItems[31] = new ShopItem(item, cat, 2.0);		
+		defaultItems[35] = new ShopItem(item, cat, 2.0);		
+		
+		
+		//Enchanted
+		item = new ItemStack(Material.DIAMOND_SWORD);
+		item.addUnsafeEnchantment(Enchantment.KNOCKBACK, 2);
+		item.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10);
+		item.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 3);
+		item.addUnsafeEnchantment(Enchantment.DURABILITY, 5);
+		defaultItems[36] = new ShopItem(item, cat, 25.0);
+		
 		
 		ItemStorage.saveItemsToFile(itemFile, "items", defaultItems);
 	}
@@ -161,6 +181,25 @@ public class ShopManager {
 	
 	public ShopItem[] getItems() {
 		return items;
+	}
+	
+	public List<ShopItem> getItems(ItemCategory cat) {
+		List<ShopItem> items = new ArrayList<ShopItem>();
+		for(ShopItem s : getItems()) {
+			if(s.getCategory()==cat) {
+				items.add(s);
+			}
+		}
+		return items;
+	}
+	
+	public ShopItem getItem(ItemCategory cat, ItemStack stack) {
+		for(ShopItem item : getItems(cat)) {
+			if(item.getItem().equals(stack)) {
+				return item;
+			}
+		}
+		return null;
 	}
 	
 	public double getPrice(ItemStack item) {
