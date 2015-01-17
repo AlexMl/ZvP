84cc7c8beb16503390b185e8beb7770144b5e722
AlexMl <alexandermlinzk@gmail.com>
2015-01-17 22:20:56 +0100

    Updated Plugin Logger

--- src/org/util/Logger/PluginOutput.java
+++ src/org/util/Logger/PluginOutput.java
@@ -8,83 +8,92 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.Plugin;
+
 
 public class PluginOutput {
-
-	private static final Logger log = Bukkit.getLogger();
+    
+    private final Logger log;
+    
+    private File logFile;
+    
+    private boolean debugMode;
+    private int loglevel;
+    
+    public PluginOutput(Plugin plugin, boolean enableDebug, int minLevel) {
 	
-	private File logFile;
+	this.log = plugin.getLogger();
 	
-	private boolean debugMode;
-	private int loglevel;
+	this.debugMode = enableDebug;
 	
-	public PluginOutput(JavaPlugin plugin, boolean enableDebug, int minLevel) {
-			
-		this.debugMode = enableDebug;
-		
-		this.loglevel = minLevel;
-		
-		this.logFile = new File(plugin.getDataFolder().getAbsolutePath() + "/pluginlog.info");
-		try {
-			this.logFile.createNewFile();
-		} catch (IOException e) {			
-			e.printStackTrace();
+	this.loglevel = minLevel;
+	
+	this.logFile = new File(plugin.getDataFolder().getAbsolutePath() + "/pluginlog.info");
+	
+	try {
+	    this.logFile.createNewFile();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	
+    }
+    
+    public boolean isDebugMode() {
+	return this.debugMode;
+    }
+    
+    public void log(String message) {
+	log(Level.INFO, message, false);
+    }
+    
+    public void log(Level level, String message, boolean debugMessage) {
+	log(level, message, debugMessage, null);
+    }
+    
+    public void log(Level level, String message, boolean debugMessage, Exception e) {
+	
+	logData(level, message, e);
+	
+	if (!debugMessage) {
+	    if (level.intValue() >= Level.INFO.intValue()) {
+		this.log.log(level, message);
+		return;
+	    }
+	    this.log.info("[" + level.getName() + "] " + message);
+	    return;
+	}
+	
+	if (debugMessage) {
+	    if (this.debugMode && level.intValue() >= this.loglevel) {
+		if (level.intValue() >= Level.INFO.intValue()) {
+		    this.log.log(level, message);
+		    return;
 		}
-	
+		this.log.info("[" + level.getName() + "] " + message);
+		return;
+	    }
 	}
+    }
+    
+    private void logData(Level level, String message, Exception exception) {
+	SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+	Date currentTime = new Date();
 	
-	
-	public boolean isDebugMode() {
-		return debugMode;
+	try {
+	    FileWriter writer = new FileWriter(this.logFile, true);
+	    
+	    if (exception != null) {
+		message += "\n" + exception.toString();
+	    }
+	    
+	    writer.write("[" + formatter.format(currentTime) + "] [" + level.getName() + "] " + message);
+	    writer.write(System.getProperty("line.separator"));
+	    writer.flush();
+	    writer.close();
+	    
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
-	
-	public void log(String message) {
-		log(null, message, false);
-	}
-	
-	public void log(Level level, String message, boolean debugMessage) {
-		log(level, message, debugMessage, null);		
-	}
-	
-	public void log(Level level, String message, boolean debugMessage, Exception e) {
-		
-		if(!debugMessage) {
-			log.info(message);	
-			return;
-		}		
-		
-		if(debugMessage) {
-			logData(level, message, e);
-			
-			if(debugMode && level.intValue()>=this.loglevel) {
-				log.info(message);
-				return;
-			}
-		}
-	}
-	
-	
-	public void logData(Level level, String message, Exception exception){
-		SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-	    Date currentTime = new Date();	    
-	  
-		try {
-			FileWriter writer = new FileWriter(logFile, true);
-			
-			if(exception!=null) {
-				message += "\n" + exception.toString();
-			}
-			
-			writer.write("[" + formatter.format(currentTime) + "] [" + level.getName() + "] " + message);			
-			writer.write(System.getProperty("line.separator"));
-			writer.flush();
-			writer.close();
-			
-		} catch (IOException e) {			
-			e.printStackTrace();
-		}	
-	}
-	
+    }
+    
 }
