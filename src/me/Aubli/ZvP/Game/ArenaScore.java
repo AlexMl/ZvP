967191da911504ee513b000630945d75b35c4a7b
AlexMl <alexandermlinzk@gmail.com>
2015-03-26 20:32:31 +0100

    Added player score separation

--- src/me/Aubli/ZvP/Game/ArenaScore.java
+++ src/me/Aubli/ZvP/Game/ArenaScore.java
@@ -1,14 +1,34 @@
 package me.Aubli.ZvP.Game;
 
+import java.util.HashMap;
+
+
 public class ArenaScore {
     
     private Arena arena;
     
     private double score;
     
-    public ArenaScore(Arena arena) {
+    private HashMap<ZvPPlayer, Double> playerScore;
+    
+    private final boolean seperated;
+    
+    public ArenaScore(Arena arena, boolean seperated) {
 	this.arena = arena;
-	this.score = 0.0;
+	this.seperated = seperated;
+	
+	if (seperated) {
+	    this.playerScore = new HashMap<ZvPPlayer, Double>();
+	    initMap();
+	} else {
+	    this.score = 0.0;
+	}
+    }
+    
+    private void initMap() {
+	for (ZvPPlayer player : getArena().getPlayers()) {
+	    this.playerScore.put(player, 0.0);
+	}
     }
     
     public double getScore() {
@@ -17,5 +37,34 @@ public class ArenaScore {
     
     public Arena getArena() {
 	return this.arena;
+    }
+    
+    public boolean isSeperated() {
+	return this.seperated;
+    }
+    
+    public void addScore(ZvPPlayer player, double score) {
+	if (isSeperated()) {
+	    this.playerScore.put(player, this.playerScore.get(player) + score);
+	} else {
+	    this.score += score;
+	}
+    }
+    
+    public void subtractScore(ZvPPlayer player, double score) {
+	if (isSeperated()) {
+	    double prevScore = this.playerScore.get(player);
+	    if (prevScore <= score) {
+		this.playerScore.put(player, 0.0);
+	    } else {
+		this.playerScore.put(player, this.playerScore.get(player) - score);
+	    }
+	} else {
+	    if (score >= this.score) {
+		this.score = 0;
+	    } else {
+		this.score -= score;
+	    }
+	}
     }
 }
