db2c52f0601c698cd05fc96dac2545d0bdf3e1f5
AlexMl <alexandermlinzk@gmail.com>
2015-04-03 00:16:04 +0200

    Added armor and dropchance to difficult system

--- src/me/Aubli/ZvP/Game/ArenaDifficulty.java
+++ src/me/Aubli/ZvP/Game/ArenaDifficulty.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 import me.Aubli.ZvP.Game.GameManager.ArenaDifficultyLevel;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Zombie;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -43,6 +45,8 @@ public class ArenaDifficulty {
 	boolean setCanPickupItems = false;
 	double maxHealth = 20D;
 	double velocity = 1D;
+	ItemStack[] armorContent = null;
+	Float dropchance = 0.5F;
 	PotionEffect potionEffect = null;
 	String name = "";
 	
@@ -51,8 +55,10 @@ public class ArenaDifficulty {
 		setBaby = false;
 		setVillager = (this.rand.nextBoolean() && this.rand.nextBoolean());
 		setCanPickupItems = false;
-		maxHealth = 18D;
+		maxHealth = 15D;
 		velocity = 0.8D;
+		armorContent = null;
+		dropchance = 0F;
 		name = "EASY";
 		break;
 	    
@@ -63,27 +69,32 @@ public class ArenaDifficulty {
 			setCanPickupItems = true;
 			maxHealth = 40D;
 			velocity = 1.2D;
+			dropchance = 0.6F;
 			break;
 		    case 1:
 			setBaby = true;
 			setCanPickupItems = false;
 			velocity = 0.75D;
+			dropchance = 0.6F;
 			break;
 		    case 2:
 			setBaby = false;
 			setCanPickupItems = true;
 			setVillager = false;
 			maxHealth = 15D;
+			dropchance = 0.4F;
 			break;
 		    case 3:
 			setBaby = false;
 			setCanPickupItems = false;
 			setVillager = true;
 			maxHealth = 30D;
+			dropchance = 0.3F;
 			break;
 		    default:
 			break;
 		}
+		armorContent = getRandomArmor();
 		name = "NORMAL";
 		break;
 	    
@@ -95,6 +106,7 @@ public class ArenaDifficulty {
 			maxHealth = 40D;
 			velocity = 1.2D;
 			potionEffect = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20 * 20, 2);
+			dropchance = 0.4F;
 			break;
 		    case 1:
 			setBaby = true;
@@ -102,6 +114,7 @@ public class ArenaDifficulty {
 			maxHealth = 30D;
 			velocity = 1D;
 			potionEffect = new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 100 * 20, 2);
+			dropchance = 0.4F;
 			break;
 		    case 2:
 			setBaby = false;
@@ -110,6 +123,7 @@ public class ArenaDifficulty {
 			maxHealth = 20D;
 			velocity = 1.2D;
 			potionEffect = new PotionEffect(PotionEffectType.HEAL, 20 * 20, 2);
+			dropchance = 0.2F;
 			break;
 		    case 3:
 			setBaby = false;
@@ -118,22 +132,90 @@ public class ArenaDifficulty {
 			maxHealth = 35D;
 			velocity = 1.1D;
 			potionEffect = new PotionEffect(PotionEffectType.SPEED, 20 * 20, 2);
+			dropchance = 0.2F;
 			break;
 		    default:
 			maxHealth = 30D;
 			setCanPickupItems = true;
+			dropchance = 0.25F;
 			break;
 		}
+		armorContent = getRandomArmor();
 		name = "HARD";
 		break;
 	}
 	
 	z.setCustomName(name);
 	z.setMaxHealth(maxHealth);
+	z.setHealth(maxHealth);
+	z.getEquipment().setArmorContents(armorContent);
+	z.getEquipment().setBootsDropChance(dropchance);
+	z.getEquipment().setChestplateDropChance(dropchance);
+	z.getEquipment().setHelmetDropChance(dropchance);
+	z.getEquipment().setItemInHandDropChance(dropchance);
+	z.getEquipment().setLeggingsDropChance(dropchance);
 	z.setVelocity(z.getVelocity().multiply(velocity));
 	z.setBaby(setBaby);
 	z.setVillager(setVillager);
 	z.setCanPickupItems(setCanPickupItems);
-	z.addPotionEffect(potionEffect, true);
+	
+	if (potionEffect != null) {
+	    z.addPotionEffect(potionEffect, true);
+	}
+    }
+    
+    private ItemStack[] getRandomArmor() {
+	ItemStack[] content = new ItemStack[4];
+	
+	int i = this.rand.nextInt(7) + getDifficulty().getLevel();
+	switch (i) {
+	    case 1:
+		content[0] = new ItemStack(Material.LEATHER_HELMET);
+		content[2] = new ItemStack(Material.IRON_LEGGINGS);
+		break;
+	    
+	    case 2:
+		content[0] = new ItemStack(Material.IRON_HELMET);
+		content[1] = new ItemStack(Material.LEATHER_CHESTPLATE);
+		content[3] = new ItemStack(Material.LEATHER_BOOTS);
+		break;
+	    
+	    case 4:
+		content[0] = new ItemStack(Material.GOLD_HELMET);
+		content[1] = new ItemStack(Material.IRON_CHESTPLATE);
+		content[2] = new ItemStack(Material.IRON_LEGGINGS);
+		break;
+	    
+	    case 6:
+		content[0] = new ItemStack(Material.GOLD_HELMET);
+		content[1] = new ItemStack(Material.DIAMOND_CHESTPLATE);
+		content[2] = new ItemStack(Material.GOLD_LEGGINGS);
+		break;
+	    
+	    case 8:
+		content[0] = new ItemStack(Material.CHAINMAIL_HELMET);
+		content[1] = new ItemStack(Material.DIAMOND_CHESTPLATE);
+		content[3] = new ItemStack(Material.DIAMOND_BOOTS);
+		break;
+	    
+	    case 9:
+		content[0] = new ItemStack(Material.CHAINMAIL_HELMET);
+		content[1] = new ItemStack(Material.IRON_CHESTPLATE);
+		content[2] = new ItemStack(Material.IRON_LEGGINGS);
+		content[3] = new ItemStack(Material.CHAINMAIL_BOOTS);
+		break;
+	    
+	    case 10:
+		content[0] = new ItemStack(Material.DIAMOND_HELMET);
+		content[1] = new ItemStack(Material.DIAMOND_CHESTPLATE);
+		content[2] = new ItemStack(Material.DIAMOND_LEGGINGS);
+		content[3] = new ItemStack(Material.DIAMOND_BOOTS);
+		break;
+	    
+	    default:
+		content = null;
+		break;
+	}
+	return content;
     }
 }
