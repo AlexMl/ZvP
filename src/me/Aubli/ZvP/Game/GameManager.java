eb43ac8947eec61615ab36030f9c75b1dd8f6e2f
AlexMl <alexandermlinzk@gmail.com>
2015-04-15 17:31:25 +0200

    Fixed #29. Players are now not allowed to join while a game is running

--- src/me/Aubli/ZvP/Game/GameManager.java
+++ src/me/Aubli/ZvP/Game/GameManager.java
@@ -339,13 +339,17 @@ public class GameManager {
     public boolean createPlayer(Player player, Arena arena, Lobby lobby) {
 	
 	if (!arena.isFull() && arena.isOnline()) {
-	    try {
-		new ZvPPlayer(player, arena, lobby);
-		SignManager.getManager().updateSigns(lobby);
-		SignManager.getManager().updateSigns(arena);
-		return true;
-	    } catch (Exception e) {
-		ZvP.getPluginLogger().log(Level.WARNING, "Error while creating Player: " + e.getMessage(), true, false, e);
+	    if (!arena.isRunning()) {
+		try {
+		    new ZvPPlayer(player, arena, lobby);
+		    SignManager.getManager().updateSigns(lobby);
+		    SignManager.getManager().updateSigns(arena);
+		    return true;
+		} catch (Exception e) {
+		    ZvP.getPluginLogger().log(Level.WARNING, "Error while creating Player: " + e.getMessage(), true, false, e);
+		    return false;
+		}
+	    } else {
 		return false;
 	    }
 	} else {
