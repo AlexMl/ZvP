7860bed368f72bef195edeb411f1ec9a4c2ed384
AlexMl <alexandermlinzk@gmail.com>
2015-04-26 11:43:19 +0200

    Added kitfile convertion

--- src/org/util/Converter/FileConverter.java
+++ src/org/util/Converter/FileConverter.java
@@ -38,68 +38,94 @@ public class FileConverter {
 	try {
 	    String localVersion = conf.getString("version");
 	    
-	    if (type == FileType.ARENAFILE) {
-		if (localVersion == null || parseVersion(localVersion) < uptadeRequired) { // Version that needs upgrade
-		    ZvP.getPluginLogger().log(Level.INFO, "Found outdated arena file(" + file.getName() + ")! Converting to " + uptadeRequired + " ...", true, false);
-		    int arenaID = conf.getInt("arena.ID");
-		    String status = conf.getString("arena.Online");
-		    
-		    int minPlayers = conf.getInt("arena.minPlayers");
-		    int maxPlayers = conf.getInt("arena.maxPlayers");
-		    int rounds = conf.getInt("arena.rounds");
-		    int waves = conf.getInt("arena.waves");
-		    int spawnRate = conf.getInt("arena.spawnRate");
-		    double saveRadius = conf.getDouble("arena.saveRadius", ZvPConfig.getDefaultSaveRadius());
-		    
-		    String world = conf.getString("arena.Location.world");
-		    int minX = conf.getInt("arena.Location.min.X");
-		    int minY = conf.getInt("arena.Location.min.Y");
-		    int minZ = conf.getInt("arena.Location.min.Z");
-		    
-		    int maxX = conf.getInt("arena.Location.max.X");
-		    int maxY = conf.getInt("arena.Location.max.Y");
-		    int maxZ = conf.getInt("arena.Location.max.Z");
-		    
-		    List<String> staticPositions = conf.getStringList("arena.Location.staticPositions");
-		    
-		    file.renameTo(new File(file.getParentFile(), file.getName() + ".old"));
-		    file.delete();
-		    file.createNewFile();
-		    
-		    conf = YamlConfiguration.loadConfiguration(file);
-		    
-		    conf.set("arena.ID", arenaID);
-		    conf.set("arena.Online", status);
-		    conf.set("arena.Difficulty", ArenaDifficultyLevel.NORMAL.name());
-		    
-		    conf.set("arena.minPlayers", minPlayers);
-		    conf.set("arena.maxPlayers", maxPlayers);
-		    conf.set("arena.rounds", rounds);
-		    conf.set("arena.waves", waves);
-		    conf.set("arena.spawnRate", spawnRate);
-		    
-		    conf.set("arena.safety.SpawnProtection.enabled", true);
-		    conf.set("arena.safety.SpawnProtection.duration", 5);
-		    conf.set("arena.safety.saveRadius", saveRadius);
-		    
-		    conf.set("arena.Location.world", world);
-		    conf.set("arena.Location.min.X", minX);
-		    conf.set("arena.Location.min.Y", minY);
-		    conf.set("arena.Location.min.Z", minZ);
-		    
-		    conf.set("arena.Location.max.X", maxX);
-		    conf.set("arena.Location.max.Y", maxY);
-		    conf.set("arena.Location.max.Z", maxZ);
-		    
-		    conf.set("arena.Location.staticPositions", staticPositions);
-		    
-		    conf.set("version", this.currentVersion);
-		    
-		    conf.save(file);
-		    ZvP.getPluginLogger().log(Level.INFO, "Updated " + file.getName() + " to " + uptadeRequired + " successfully!", true, false);
-		    return true;
+	    if (localVersion == null || parseVersion(localVersion) < uptadeRequired) { // Version that needs upgrade
+	    
+		switch (type) {
+		    case ARENAFILE:
+			ZvP.getPluginLogger().log(Level.INFO, "Found outdated arena file(" + file.getName() + ")! Converting to " + uptadeRequired + " ...", true, false);
+			int arenaID = conf.getInt("arena.ID");
+			String status = conf.getString("arena.Online");
+			
+			int minPlayers = conf.getInt("arena.minPlayers");
+			int maxPlayers = conf.getInt("arena.maxPlayers");
+			int rounds = conf.getInt("arena.rounds");
+			int waves = conf.getInt("arena.waves");
+			int spawnRate = conf.getInt("arena.spawnRate");
+			double saveRadius = conf.getDouble("arena.saveRadius", ZvPConfig.getDefaultSaveRadius());
+			
+			String world = conf.getString("arena.Location.world");
+			int minX = conf.getInt("arena.Location.min.X");
+			int minY = conf.getInt("arena.Location.min.Y");
+			int minZ = conf.getInt("arena.Location.min.Z");
+			
+			int maxX = conf.getInt("arena.Location.max.X");
+			int maxY = conf.getInt("arena.Location.max.Y");
+			int maxZ = conf.getInt("arena.Location.max.Z");
+			
+			List<String> staticPositions = conf.getStringList("arena.Location.staticPositions");
+			
+			file.renameTo(new File(file.getParentFile(), file.getName() + ".old"));
+			file.delete();
+			file.createNewFile();
+			
+			conf = YamlConfiguration.loadConfiguration(file);
+			
+			conf.set("arena.ID", arenaID);
+			conf.set("arena.Online", status);
+			conf.set("arena.Difficulty", ArenaDifficultyLevel.NORMAL.name());
+			
+			conf.set("arena.minPlayers", minPlayers);
+			conf.set("arena.maxPlayers", maxPlayers);
+			conf.set("arena.rounds", rounds);
+			conf.set("arena.waves", waves);
+			conf.set("arena.spawnRate", spawnRate);
+			
+			conf.set("arena.safety.SpawnProtection.enabled", true);
+			conf.set("arena.safety.SpawnProtection.duration", 5);
+			conf.set("arena.safety.saveRadius", saveRadius);
+			
+			conf.set("arena.Location.world", world);
+			conf.set("arena.Location.min.X", minX);
+			conf.set("arena.Location.min.Y", minY);
+			conf.set("arena.Location.min.Z", minZ);
+			
+			conf.set("arena.Location.max.X", maxX);
+			conf.set("arena.Location.max.Y", maxY);
+			conf.set("arena.Location.max.Z", maxZ);
+			
+			conf.set("arena.Location.staticPositions", staticPositions);
+			
+			conf.set("version", this.currentVersion);
+			
+			conf.save(file);
+			ZvP.getPluginLogger().log(Level.INFO, "Updated " + file.getName() + " to " + uptadeRequired + " successfully!", true, false);
+			return true;
+			
+		    case KITFILE:
+			ZvP.getPluginLogger().log(Level.INFO, "Found outdated kit file(" + file.getName() + ")! Converting to " + uptadeRequired + " ...", true, false);
+			String name = conf.getString("name");
+			boolean enabled = conf.getBoolean("enabled", true);
+			String icon = conf.getString("icon");
+			List<String> itemList = conf.getStringList("items");
+			
+			file.renameTo(new File(file.getParentFile(), file.getName() + ".old"));
+			file.delete();
+			file.createNewFile();
+			
+			conf = YamlConfiguration.loadConfiguration(file);
+			conf.set("name", name);
+			conf.set("enabled", enabled);
+			conf.set("price", 0.0);
+			conf.set("icon", icon);
+			conf.set("items", itemList);
+			conf.set("version", this.currentVersion);
+			return true;
+			
+		    default:
+			return false;
 		}
 	    }
+	    
 	} catch (IOException e) {
 	    ZvP.getPluginLogger().log(Level.WARNING, "Failed saving converted file for " + file.getAbsolutePath() + "!", true, false, e);
 	    return false;
