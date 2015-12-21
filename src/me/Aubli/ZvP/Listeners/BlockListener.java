415c6c922dbdb9803f2e278c43a67fd94b9a91bb
AlexMl <alexandermlinzk@gmail.com>
2015-12-21 21:02:03 +0100

    Check if block with sign is removed

--- src/me/Aubli/ZvP/Listeners/BlockListener.java
+++ src/me/Aubli/ZvP/Listeners/BlockListener.java
@@ -3,6 +3,7 @@ package me.Aubli.ZvP.Listeners;
 import me.Aubli.ZvP.ZvP;
 import me.Aubli.ZvP.ZvPCommands;
 import me.Aubli.ZvP.Game.GameManager;
+import me.Aubli.ZvP.Sign.ISign;
 import me.Aubli.ZvP.Sign.SignManager;
 import me.Aubli.ZvP.Translation.MessageKeys.error;
 import me.Aubli.ZvP.Translation.MessageKeys.manage;
@@ -49,9 +50,31 @@ public class BlockListener implements Listener {
 	
 	if (event.getBlock().getState() instanceof Sign) {
 	    Location signLoc = event.getBlock().getLocation().clone();
+	    
 	    if (this.sm.isZVPSign(signLoc)) {
 		if (this.eventPlayer.hasPermission("zvp.manage.sign")) {
 		    boolean success = this.sm.removeSign(signLoc);
+		    if (success) {
+			this.eventPlayer.sendMessage(MessageManager.getMessage(manage.sign_removed));
+			return;
+		    } else {
+			this.eventPlayer.sendMessage(MessageManager.getMessage(error.sign_remove));
+			return;
+		    }
+		} else {
+		    event.setCancelled(true);
+		    ZvPCommands.commandDenied(this.eventPlayer);
+		    return;
+		}
+	    }
+	}
+	
+	if (this.sm.isSignBlock(event.getBlock().getLocation())) {
+	    ISign sign = this.sm.getAttachedSign(event.getBlock().getLocation());
+	    
+	    if (sign != null && this.sm.isZVPSign(sign.getLocation())) {
+		if (this.eventPlayer.hasPermission("zvp.manage.sign")) {
+		    boolean success = this.sm.removeSign(sign.getID());
 		    if (success) {
 			this.eventPlayer.sendMessage(MessageManager.getMessage(manage.sign_removed));
 			return;
