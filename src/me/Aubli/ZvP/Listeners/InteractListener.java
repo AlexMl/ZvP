04955153f91eb0f93e15eb9f2886fbe00d8348cf
AlexMl <alexandermlinzk@gmail.com>
2016-02-13 01:14:02 +0100

    Disallow shop in arena standby

--- src/me/Aubli/ZvP/Listeners/InteractListener.java
+++ src/me/Aubli/ZvP/Listeners/InteractListener.java
@@ -7,6 +7,7 @@ import java.util.List;
 import me.Aubli.ZvP.ZvP;
 import me.Aubli.ZvP.ZvPCommands;
 import me.Aubli.ZvP.Game.Arena;
+import me.Aubli.ZvP.Game.GameEnums.ArenaStatus;
 import me.Aubli.ZvP.Game.GameManager;
 import me.Aubli.ZvP.Game.ZvPPlayer;
 import me.Aubli.ZvP.Shop.ShopItem;
@@ -37,35 +38,35 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 
 public class InteractListener implements Listener {
-    
+
     private HashMap<Action, Location> clickLoc = new HashMap<Action, Location>();
     private static List<Location> locationList = new ArrayList<Location>();
-    
+
     private SignManager sm = SignManager.getManager();
-    
+
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerInteract(PlayerInteractEvent event) {
-	
+
 	Player eventPlayer = event.getPlayer();
-	
+
 	if (event.getItem() != null) {
 	    if (ZvP.equalsItemStack(event.getItem(), ZvP.getTool(""))) {
 		if (eventPlayer.hasPermission("zvp.manage.arena")) {
-		    
+
 		    String toolString = ChatColor.stripColor(event.getItem().getItemMeta().getLore().get(0));
 		    event.setCancelled(true);
-		    
+
 		    if (toolString.equals(ZvP.ADDARENA_SINGLE)) {
 			if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			    this.clickLoc.put(event.getAction(), event.getClickedBlock().getLocation().clone());
 			    eventPlayer.sendMessage(MessageManager.getMessage(manage.right_saved));
 			}
-			
+
 			if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
 			    this.clickLoc.put(event.getAction(), event.getClickedBlock().getLocation().clone());
 			    eventPlayer.sendMessage(MessageManager.getMessage(manage.left_saved));
 			}
-			
+
 			if (this.clickLoc.containsKey(Action.RIGHT_CLICK_BLOCK) && this.clickLoc.containsKey(Action.LEFT_CLICK_BLOCK)) {
 			    Arena arena = GameManager.getManager().addArena(this.clickLoc.get(Action.LEFT_CLICK_BLOCK), this.clickLoc.get(Action.RIGHT_CLICK_BLOCK));
 			    if (arena != null) {
@@ -73,14 +74,14 @@ public class InteractListener implements Listener {
 			    } else {
 				eventPlayer.sendMessage(MessageManager.getMessage(error.arena_place));
 			    }
-			    
+
 			    this.clickLoc.clear();
 			}
-			
+
 		    } else if (toolString.equals(ZvP.ADDARENA_POLYGON)) {
 			locationList.add(event.getClickedBlock().getLocation().clone());
 			eventPlayer.sendMessage(MessageManager.getFormatedMessage(manage.position_saved_poly, locationList.size()));
-			
+
 		    } else if (toolString.equals(ZvP.ADDPOSITION)) {
 			for (Arena arena : GameManager.getManager().getArenas()) {
 			    if (arena.containsLocation(eventPlayer.getLocation())) {
@@ -95,7 +96,7 @@ public class InteractListener implements Listener {
 			}
 			eventPlayer.sendMessage(MessageManager.getMessage(manage.position_not_in_arena));
 		    }
-		    
+
 		} else {
 		    ZvP.removeTool(eventPlayer);
 		    ZvPCommands.commandDenied(eventPlayer);
@@ -103,7 +104,7 @@ public class InteractListener implements Listener {
 		return;
 	    }
 	}
-	
+
 	if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 	    if (this.sm.isZVPSign(event.getClickedBlock().getLocation())) {
 		if (this.sm.getType(event.getClickedBlock().getLocation()) == SignType.INTERACT_SIGN) { // Player join per Sign
@@ -113,7 +114,7 @@ public class InteractListener implements Listener {
 			    if (sign.getArena() != null) {
 				if (sign.getArena().isOnline()) {
 				    boolean success = GameManager.getManager().createPlayer(eventPlayer, sign.getArena(), sign.getLobby());
-				    
+
 				    if (!success) {
 					event.setCancelled(true);
 					eventPlayer.sendMessage(MessageManager.getMessage(arena.not_ready));
@@ -143,26 +144,26 @@ public class InteractListener implements Listener {
 		    if (GameManager.getManager().isInGame(eventPlayer)) {
 			if (eventPlayer.hasPermission("zvp.play")) {
 			    ZvPPlayer player = GameManager.getManager().getPlayer(eventPlayer);
-			    
-			    if (!player.getArena().isWaiting()) { // TODO TEST
+
+			    if (!player.getArena().isWaiting() && player.getArena().getStatus() != ArenaStatus.STANDBY) {
 				if (player.getArena().getArenaMode().allowPlayerInteraction(player)) {
 				    event.setCancelled(true);
-				    
+
 				    ShopSign shopSign = (ShopSign) this.sm.getSign(event.getClickedBlock().getLocation());
 				    ItemCategory cat = shopSign.getCategory();
-				    
+
 				    Inventory shopInv = Bukkit.createInventory(eventPlayer, ((int) Math.ceil(ShopManager.getManager().getItems(cat).size() / 9.0)) * 9, "Items: " + cat.toString());
-				    
+
 				    for (ShopItem shopItem : ShopManager.getManager().getItems(cat)) {
-					
+
 					ItemStack item = shopItem.getItem();
 					ItemMeta meta = item.getItemMeta();
 					List<String> lore = new ArrayList<String>();
-					
+
 					lore.add("Category: " + shopItem.getCategory().toString());
 					lore.add(player.getArena().getScore().getScore(player) >= shopItem.getBuyPrice() ? (ChatColor.GREEN + "Cost: " + shopItem.getBuyPrice()) : (ChatColor.RED + "Cost: " + shopItem.getBuyPrice()));
 					lore.add(ChatColor.GOLD + "Refund: " + shopItem.getSellPrice());
-					
+
 					meta.setLore(lore);
 					item.setItemMeta(meta);
 					shopInv.addItem(item);
@@ -191,11 +192,11 @@ public class InteractListener implements Listener {
 	    }
 	}
     }
-    
+
     public static void clearPositionList() {
 	locationList.clear();
     }
-    
+
     public static Arena createArenaFromList() {
 	return GameManager.getManager().addArena(locationList);
     }
