69b372b07bf168a4b10de6328916adcbc6ca3377
AlexMl <alexandermlinzk@gmail.com>
2016-03-20 16:20:55 +0100

    Fixed npe

--- src/me/Aubli/ZvP/Game/ArenaParts/ArenaDifficulty.java
+++ src/me/Aubli/ZvP/Game/ArenaParts/ArenaDifficulty.java
@@ -14,49 +14,49 @@ import org.bukkit.potion.PotionEffectType;
 
 
 public class ArenaDifficulty {
-    
+
     private Arena arena;
-    
+
     private ArenaDifficultyLevel arenaLevel;
-    
+
     private Random rand;
-    
+
     public ArenaDifficulty(Arena arena, ArenaDifficultyLevel level) {
 	this.arena = arena;
 	this.arenaLevel = level;
-	
+
 	this.rand = new Random();
     }
-    
+
     public Arena getArena() {
 	return this.arena;
     }
-    
+
     public ArenaDifficultyLevel getDifficulty() {
 	return this.arenaLevel;
     }
-    
+
     public double getExpFactor() {
 	return (getDifficulty().getLevel() + 3.0) / 2.0;
     }
-    
+
     public double getMoneyFactor() {
 	return (getDifficulty().getLevel() + 1.0) / 2.0;
     }
-    
+
     public double getZombieStrengthFactor() {
 	return ((((getArena().getCurrentRound() * 5.0 + getArena().getCurrentWave()) - 6.0) + 5.0) / 100.0 + 1.0) * ((getDifficulty().getLevel() + 1.0) / 2.0);
     }
-    
+
     public double getZombieHealthFactor() {
 	return ((((getArena().getCurrentRound() * 5.0 + getArena().getCurrentWave()) - 6.0) + 0.0) / 100.0 + 1.0) * ((getDifficulty().getLevel() + 1.0) / 2.0);
     }
-    
+
     public void customizeEntity(Entity zombie) {
 	Zombie z = (Zombie) zombie;
 	z.setRemoveWhenFarAway(false);
 	z.setTarget(getArena().getRandomPlayer().getPlayer());
-	
+
 	boolean setBaby = false;
 	boolean setVillager = false;
 	boolean setCanPickupItems = false;
@@ -64,17 +64,17 @@ public class ArenaDifficulty {
 	ItemStack[] armorContent = null;
 	Float dropchance = 0.25F;
 	PotionEffect potionEffect = null;
-	
+
 	switch (getDifficulty().getLevel()) {
 	    case 1:
 		setBaby = false;
 		setVillager = (this.rand.nextBoolean() && this.rand.nextBoolean());
 		setCanPickupItems = false;
 		maxHealth = 15D;
-		armorContent = null;
+		armorContent = new ItemStack[4];
 		dropchance = 0F;
 		break;
-	    
+
 	    case 2:
 		switch (this.rand.nextInt(7)) {
 		    case 0:
@@ -82,14 +82,14 @@ public class ArenaDifficulty {
 			setCanPickupItems = true;
 			maxHealth = 40D;
 			dropchance = 0.35F;
-			armorContent = null;
+			armorContent = new ItemStack[4];
 			break;
 		    case 1:
 			setBaby = true;
 			setCanPickupItems = false;
 			maxHealth = 20D;
 			dropchance = 0.35F;
-			armorContent = null;
+			armorContent = new ItemStack[4];
 			break;
 		    case 2:
 			setBaby = false;
@@ -111,7 +111,7 @@ public class ArenaDifficulty {
 			break;
 		}
 		break;
-	    
+
 	    case 3:
 		switch (this.rand.nextInt(7)) {
 		    case 0:
@@ -151,11 +151,11 @@ public class ArenaDifficulty {
 		setCanPickupItems = true;
 		break;
 	}
-	
+
 	if (getArena().getConfig().isIncreaseDifficulty()) {
 	    maxHealth *= getZombieHealthFactor();
 	}
-	
+
 	z.setMaxHealth(Math.ceil(maxHealth));
 	z.setHealth(maxHealth);
 	z.getEquipment().setArmorContents(armorContent);
@@ -167,62 +167,63 @@ public class ArenaDifficulty {
 	z.setBaby(setBaby);
 	z.setVillager(setVillager);
 	z.setCanPickupItems(setCanPickupItems);
-	
+
 	if (potionEffect != null) {
 	    z.addPotionEffect(potionEffect, true);
 	}
     }
-    
+
     private ItemStack[] getRandomArmor() {
 	ItemStack[] content = new ItemStack[4];
-	
+
 	int i = this.rand.nextInt(10) + getDifficulty().getLevel();
 	switch (i) {
 	    case 1:
 		content[0] = new ItemStack(Material.LEATHER_HELMET);
 		content[2] = new ItemStack(Material.IRON_LEGGINGS);
 		break;
-	    
+
 	    case 2:
 		content[0] = new ItemStack(Material.IRON_HELMET);
 		content[1] = new ItemStack(Material.LEATHER_CHESTPLATE);
 		content[3] = new ItemStack(Material.LEATHER_BOOTS);
 		break;
-	    
+
 	    case 4:
 		content[1] = new ItemStack(Material.IRON_CHESTPLATE);
 		content[2] = new ItemStack(Material.IRON_LEGGINGS);
 		break;
-	    
+
 	    case 6:
 		content[0] = new ItemStack(Material.GOLD_HELMET);
 		content[1] = new ItemStack(Material.DIAMOND_CHESTPLATE);
 		break;
-	    
+
 	    case 10:
 		content[0] = new ItemStack(Material.CHAINMAIL_HELMET);
 		content[1] = new ItemStack(Material.DIAMOND_CHESTPLATE);
 		content[3] = new ItemStack(Material.DIAMOND_BOOTS);
 		break;
-	    
+
 	    case 11:
 		content[0] = new ItemStack(Material.CHAINMAIL_HELMET);
 		content[1] = new ItemStack(Material.IRON_CHESTPLATE);
 		content[2] = new ItemStack(Material.IRON_LEGGINGS);
 		content[3] = new ItemStack(Material.CHAINMAIL_BOOTS);
 		break;
-	    
+
 	    case 12:
 		content[0] = new ItemStack(Material.DIAMOND_HELMET);
 		content[1] = new ItemStack(Material.DIAMOND_CHESTPLATE);
 		content[2] = new ItemStack(Material.DIAMOND_LEGGINGS);
 		content[3] = new ItemStack(Material.DIAMOND_BOOTS);
 		break;
-	    
+
 	    default:
 		content = null;
 		break;
 	}
+
 	return content;
     }
 }
