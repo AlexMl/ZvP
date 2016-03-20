a9f73836032294edc5fb352933212d348ac5999f
AlexMl <alexandermlinzk@gmail.com>
2016-03-20 17:04:31 +0100

    Disable potions in kits

--- src/me/Aubli/ZvP/Kits/KCustomKit.java
+++ src/me/Aubli/ZvP/Kits/KCustomKit.java
@@ -14,21 +14,21 @@ import org.util.File.ItemStorageUtil.ItemStorage;
 
 
 public class KCustomKit implements IZvPKit, Comparable<IZvPKit> {
-    
+
     private final File kitFile;
-    
+
     private final String name;
-    
+
     private final String permNode;
-    
+
     private final ItemStack icon;
-    
+
     private final double price;
-    
+
     private final ItemStack[] items;
-    
+
     private final boolean enabled;
-    
+
     public KCustomKit(String path, String name, ItemStack icon, double price, ItemStack[] content) {
 	this.name = name;
 	this.icon = icon;
@@ -36,15 +36,15 @@ public class KCustomKit implements IZvPKit, Comparable<IZvPKit> {
 	this.items = content;
 	this.enabled = true;
 	this.permNode = "zvp.play"; // INFO: Magic number(permission node)
-	
+
 	this.kitFile = new File(path + "/" + name + ".yml");
 	FileConfiguration kitConfig = YamlConfiguration.loadConfiguration(this.kitFile);
-	
+
 	if (!this.kitFile.exists()) {
-	    
+
 	    kitConfig.options().header("This is the config file used in ZvP to store a custom kit.\n\n'name:' The name of the kit\n'enabled:' State of the kit\n'permission:' The permission to use this kit. If kept on default permission nothing changes.\n'price:' The price of the kit if economy is used\n'icon:' An item used as an icon\n\n" + "'id:' The id describes the item material. A list of all items can be found here: https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/Material.html\n" + "'amount:' The amount of the item (Should be 1!)\n" + "'data:' Used by potions\n" + "'ench: {}' A list of enchantings (ench: {ENCHANTMENT:LEVEL}). A list of enchantments can be found here:\n https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/enchantments/Enchantment.html\n");
 	    kitConfig.options().copyHeader(true);
-	    
+
 	    kitConfig.set("name", name);
 	    kitConfig.set("enabled", true);
 	    kitConfig.set("permission", this.permNode);
@@ -53,7 +53,7 @@ public class KCustomKit implements IZvPKit, Comparable<IZvPKit> {
 	    kitConfig.addDefault("version", ZvP.getInstance().getDescription().getVersion());
 	    kitConfig.options().copyDefaults(true);
 	    ItemStorage.saveItemsToFile(this.kitFile, "items", content);
-	    
+
 	    try {
 		kitConfig.load(this.kitFile);
 		kitConfig.save(this.kitFile);
@@ -62,12 +62,12 @@ public class KCustomKit implements IZvPKit, Comparable<IZvPKit> {
 	    }
 	}
     }
-    
+
     public KCustomKit(File kitFile) {
-	
+
 	this.kitFile = kitFile;
 	FileConfiguration kitConfig = YamlConfiguration.loadConfiguration(kitFile);
-	
+
 	this.name = kitConfig.getString("name");
 	this.enabled = kitConfig.getBoolean("enabled");
 	this.permNode = kitConfig.getString("permission");
@@ -75,7 +75,7 @@ public class KCustomKit implements IZvPKit, Comparable<IZvPKit> {
 	this.price = kitConfig.getDouble("price");
 	this.items = parseItemStack(kitConfig.getList("items"));
     }
-    
+
     private ItemStack[] parseItemStack(List<?> itemList) {
 	try {
 	    return ItemStorage.getItemsFromFile(itemList);
@@ -84,46 +84,46 @@ public class KCustomKit implements IZvPKit, Comparable<IZvPKit> {
 	}
 	return null;
     }
-    
+
     private ItemStack parseIcon(String itemString) {
 	return new ItemStack(Material.getMaterial(itemString));
     }
-    
+
     @Override
     public void delete() {
 	this.kitFile.delete();
     }
-    
+
     @Override
     public boolean isEnabled() {
 	return this.enabled;
     }
-    
+
     @Override
     public String getName() {
 	return this.name;
     }
-    
+
     @Override
     public String getPermissionNode() {
 	return this.permNode;
     }
-    
+
     @Override
     public ItemStack getIcon() {
 	return this.icon;
     }
-    
+
     @Override
     public double getPrice() {
 	return this.price;
     }
-    
+
     @Override
     public ItemStack[] getContents() {
 	return this.items.clone();
     }
-    
+
     @Override
     public int compareTo(IZvPKit o) {
 	return getName().compareTo(o.getName());
